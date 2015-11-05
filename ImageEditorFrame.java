c831b90a8c9cdb3fd323ec018cad25db80fece55
Denis Nguyen <you@example.com>
2015-11-05 13:48:55 +0100

    alles fertig

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -1,53 +1,65 @@
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.*;
-import java.awt.image.*;
-import java.io.*;
-import javax.imageio.*;
-public class ImageEditorFrame extends JFrame{ 
-	public ImageEditorFrame(){
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-panel = new ImageEditorPanel();
-add(panel);
-		setDefaultCloseOperation(EXIT_ON_CLOSE); 
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+public class ImageEditorFrame1 extends JFrame{ 
+	public JPanel panel;
+
+	public ImageEditorFrame1(){
+		panel = new ImageEditorPanel();
+		add(panel);
+		
+		//creating new Menubar
+		JMenuBar menuBar = new JMenuBar(); 
+		setJMenuBar(menuBar);
+		JMenu menuFile = new JMenu("File"); 
+		menuBar.add(menuFile);
+		JMenuItem menuItemOpen = new JMenuItem("Open"); 
+		menuFile.add(menuItemOpen); 
+		JMenuItem dummy = new JMenuItem("Dummy"); 
+		menuFile.add(dummy); 
+		menuItemOpen.addActionListener(e ->onOpen());
+		dummy.addActionListener(e ->setDummyImage());
+
+		setTitle("BildEditor");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		setSize(400, 300);
 		setVisible(true);
-		setTitle("BildEditor");
-}
 
-	private void createMenuBar(){
-	
-	
-JMenuBar menuBar = new JMenuBar(); 
-setJMenuBar(menuBar);
-JMenu menuFile = new JMenu("File"); 
-menuBar.add(menuFile);
-JMenuItem menuItemOpen = new JMenuItem("Open"); 
-menuFile.add(menuItemOpen); 
-menuItemOpen.addActionListener(new ActionListener() {
-public void ActionPerformed(ActionEvent e ){
-onOpen();
-}
+}	
+	private void setDummyImage(){
+		BufferedImage bufferedImage = new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+		Graphics g = bufferedImage.getGraphics();
+		g.setColor(Color.YELLOW);
+		g.fillOval(10, 10, 380, 280);
+		((ImageEditorPanel) panel).setImage(bufferedImage);
+	}
+		
+		private void onOpen(){
+			   try{
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.showOpenDialog(this);
+			File file = fileChooser.getSelectedFile(); 
+			BufferedImage image = ImageIO.read(file); 
+			((ImageEditorPanel) panel).setImage(image);
+			   }
+			   catch(IOException e){
+			JOptionPane.showMessageDialog(this,
+			"Die Datei konnte nicht geöffnet werden");
+			   }
+	}
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
-} 
-}
-); 
-}
-private void onOpen(){ JOptionPane.showMessageDialog(this, "Open Selected");
-}
-private void setDummyImage(){
- BufferedImage bufferedImage =
- new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
- Graphics g = bufferedImage.getGraphics();
- g.setColor(Color.YELLOW);
- g.fillOval(10, 10, 380, 280);
- panel.setImage(bufferedImage);
-}
 	public static void main (String [] args){
-		new ImageEditorFrame();
+		new ImageEditorFrame1();
 	}
 }
